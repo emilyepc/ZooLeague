608bd66aa4b2b55812483c22c1ee254565efc29d
c233033483 <145998880+c233033483@users.noreply.github.com>
2025-03-11 21:41:32 +0000

    Booost

--- Assets/overallboost.cs
+++ Assets/overallboost.cs
@@ -3,9 +3,56 @@ using UnityEngine.Serialization;
 
 public class overallboost : MonoBehaviour
 {
-    // Public field to assign the DraggableItem script via the Inspector
+    public RosterStatUpdating rosterStatUpdating;
     public DraggablePlayer draggablePlayer;
 
+    void Update()
+    {
+        GameObject correspondingPlayer;
+
+        if (rosterStatUpdating.playerNameText.text == "Francis The Frog")
+        {
+            correspondingPlayer = GameObject.Find("Francis The Frog");   
+            draggablePlayer = correspondingPlayer.GetComponent<DraggablePlayer>();
+        } 
+        else if (rosterStatUpdating.playerNameText.text == "Danny The Dog")
+        {
+            correspondingPlayer = GameObject.Find("Danny The Dog");
+            draggablePlayer = correspondingPlayer.GetComponent<DraggablePlayer>();
+        }
+        else if (rosterStatUpdating.playerNameText.text == "Felicity The Frog")
+        {
+            correspondingPlayer = GameObject.Find("Felicity The Frog");
+            draggablePlayer = correspondingPlayer.GetComponent<DraggablePlayer>();
+        }
+        else if (rosterStatUpdating.playerNameText.text == "Florence The Fox")
+        {
+            correspondingPlayer = GameObject.Find("Florence The Fox");
+            draggablePlayer = correspondingPlayer.GetComponent<DraggablePlayer>();
+        }
+        else if (rosterStatUpdating.playerNameText.text == "Freddie The Fog")
+        {
+            correspondingPlayer = GameObject.Find("Freddie The Fog");
+            draggablePlayer = correspondingPlayer.GetComponent<DraggablePlayer>();
+        }
+        else if (rosterStatUpdating.playerNameText.text == "Darcy The Dog")
+        {
+            correspondingPlayer = GameObject.Find("Darcy The Dog");
+            draggablePlayer = correspondingPlayer.GetComponent<DraggablePlayer>();
+        }
+        else if (rosterStatUpdating.playerNameText.text == "Honey The Hare")
+        {
+            correspondingPlayer = GameObject.Find("Honey The Hare");
+            draggablePlayer = correspondingPlayer.GetComponent<DraggablePlayer>();
+        }
+        else if (rosterStatUpdating.playerNameText.text == "Harvey The Hare")
+        {
+            correspondingPlayer = GameObject.Find("Harvey The Hare");
+            draggablePlayer = correspondingPlayer.GetComponent<DraggablePlayer>();
+        }
+
+    }
+    
     public void OnButtonPressed()
     {
         if (draggablePlayer != null)
